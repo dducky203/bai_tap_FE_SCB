ce5d961880027d92dd2dacd9d1b4deb06f276994
Ngọc Dươngg <145637328+dducky203@users.noreply.github.com>
2023-10-20 14:52:55 +0700

    project final

---
+++ web bán hàng/javascript/script_hp.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.tm-row').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+    
+});

d4232c87aa4119838325b797e7029ed321c0ab90
Ngọc Dươngg <145637328+dducky203@users.noreply.github.com>
2023-11-01 20:51:17 +0700

    final project

---
+++ foodshop web/javascript/script_hp.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.tm-row').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 5000,
+      });
+
+      $(window).scroll(function () { 
+        if ($(this).scrollTop()) {
+          $('#back-top').fadeIn();
+        } else {
+          $('#back-top').fadeOut();
+        }
+      });
+
+      $('#back-top').click(function () { 
+        $('html, body').animate({
+          scrollTop: 0},
+          50);
+      });
+
+      $('.another-logo').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4500,
+      });
+    
+});
